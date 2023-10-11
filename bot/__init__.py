d1e7adc16a174bbe9850c0222a6d1ca2209c1650
Diffusion 123 <edithcloud2@gmail.com>
2023-10-11 17:42:40 +0000

    Replace __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -794,12 +794,6 @@ else:
 log_info("Creating client from BOT_TOKEN")
 bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000,
                parse_mode=enums.ParseMode.HTML).start()
-               bot_loop = bot.loop
-               bot_name = bot.me.username
-               scheduler = AsyncIOScheduler(timezone=str(get_localzone()), event_loop=bot_loop)
-               log_info("Creating client from BOT_TOKEN")
-bot = tgClient('bot', TELEGRAM_API, TELEGRAM_HASH, bot_token=BOT_TOKEN, workers=1000,
-               parse_mode=enums.ParseMode.HTML).start()
 bot_loop = bot.loop
 bot_name = bot.me.username
 scheduler = AsyncIOScheduler(timezone=str(get_localzone()), event_loop=bot_loop)
