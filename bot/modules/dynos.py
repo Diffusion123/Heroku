96aaa533fdc8d7230b5028ecaeec44147ee1b2e2
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-10-19 02:48:04 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -25,7 +25,7 @@ from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
 api_key = config_dict["HEROKU_API_KEY"]
 app_name = config_dict["HEROKU_APP_NAME"]
 
-def restart_heroku_dynos(api_key, app_name):
+def dynos(api_key, app_name):
     # Form the URL for restarting dynos
     url = f'https://api.heroku.com/apps/{app_name}/dynos'
 
