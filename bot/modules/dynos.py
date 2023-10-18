612ac4a9c060b340bda770c9ed7c04d630e7c7fb
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-10-19 01:38:40 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -15,12 +15,18 @@ from os import path as ospath, getcwd
 from pyrogram.handlers import MessageHandler 
 from pyrogram.filters import command
 
-from bot import bot, dispatcher, HEROKU_APP_NAME, HEROKU_API_KEY
+from bot import bot, con HEROKU_APP_NAME, HEROKU_API_KEY
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
 
 async def dynos(client, message):
+    if HEROKU_API_KEY is not None and HEROKU_APP_NAME is not None:
+        Heroku = heroku3.from_key(HEROKU_API_KEY)
+        app = Heroku.app(HEROKU_APP_NAME)
+    else:
+        sendMessage(message, "Please insert your HEROKU_APP_NAME and HEROKU_API_KEY in Vars")
+            
     url = f'https://api.heroku.com/apps/{HEROKU_APP_NAME}/dynos'
 
     headers = {
