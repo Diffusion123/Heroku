564c6ea62e179d5fc1f2e9eaa4b2636084009edb
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-10-19 02:02:28 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -3,8 +3,10 @@ import requests
 import aiohttp
 import asyncio
 from subprocess import run as srun, check_output
-try: import heroku3
-except ModuleNotFoundError: srun("pip install heroku3", capture_output=True, shell=True)
+try:
+    import heroku3
+except ModuleNotFoundError:
+    srun("pip install heroku3", capture_output=True, shell=True)
 import heroku3
 from re import search as re_search
 from shlex import split as ssplit
@@ -12,7 +14,7 @@ from aiofiles import open as aiopen
 from aiofiles.os import remove as aioremove, path as aiopath, mkdir
 from os import path as ospath, getcwd
 
-from pyrogram.handlers import MessageHandler 
+from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
 
 from bot import bot, config_dict, DATABASE_URL
@@ -21,28 +23,28 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
 
 async def dynos(client, message):
-    Heroku = heroku3.from_key(HEROKU_API_ID)
-    app = Heroku.app(HEROKU_APP_NAME)
-    url = f'https://api.heroku.com/apps/{HEROKU_APP_NAME}/dynos'
+    Heroku = heroku3.from_key(config_dict['HEROKU_API_KEY'])
+    app = Heroku.app(config_dict['HEROKU_APP_NAME'])
+    url = f'https://api.heroku.com/apps/{config_dict["HEROKU_APP_NAME"]}/dynos'
 
     headers = {
         'Accept': 'application/vnd.heroku+json; version=3',
-        'Authorization': f'Bearer {HEROKU_API_KEY}',
+        'Authorization': f'Bearer {config_dict["HEROKU_API_KEY"]}',
         'Content-Type': 'application/json',
     }
-    
+
     details = await sendMessage(message, '<i>Fetching Heroku Credentials ...</i>')
-    
+
     async with aiohttp.ClientSession(headers=headers) as session:
         async with session.get(url) as response:
             dynos = await response.json()
-            
+
             for dyno in dynos:
                 dyno_id = dyno['id']
-                restart_url = f'https://api.heroku.com/apps/{HEROKU_APP_NAME}/dynos/{dyno_id}'
-                
-                async with aiohttp.ClientSession(headers={'Authorization': f'Bearer {HEROKU_API_ID}'}) as session:
+                restart_url = f'https://api.heroku.com/apps/{config_dict["HEROKU_APP_NAME"]}/dynos/{dyno_id}'
+
+                async with aiohttp.ClientSession(headers={'Authorization': f'Bearer {config_dict["HEROKU_API_KEY"]}'}) as session:
                     async with session.delete(restart_url) as response:
                         return await editMessage(details, "Dynos Restarted Successfully to 0 hours")
-          
+
 bot.add_handler(MessageHandler(dynos, filters=command(BotCommands.DynosCommand) & CustomFilters.authorized & ~CustomFilters.blacklisted))
