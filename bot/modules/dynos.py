dd63337c65bd6e70e71694770a48c5887ed0ecdf
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-10-22 13:49:57 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python3
 import requests
-import aiohttp
-import json
-import asyncio
-from subprocess import run as srun, check_output
+from subprocess import run as srun
+
 try:
     import heroku3
 except ModuleNotFoundError:
     srun("pip install heroku3", capture_output=True, shell=True)
-import heroku3
-from re import search as re_search
-from shlex import split as ssplit
-from aiofiles import open as aiopen
-from aiofiles.os import remove as aioremove, path as aiopath, mkdir
-from os import path as ospath, getcwd
 
+import heroku3
 from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
 
@@ -23,18 +16,18 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import editMessage, sendMessage
 
-api_key = config_dict["HEROKU_API_KEY"]
-app_name = config_dict["HEROKU_APP_NAME"]
+def restart_dynos(_, message):
+    reply = sendMessage(message, "Restarting Dynos...")
+    api_key = config_dict["HEROKU_API_KEY"]
+    app_name = config_dict["HEROKU_APP_NAME"]
 
-def dynos(api_key, app_name):
     # Form the URL for restarting dynos
     url = f'https://api.heroku.com/apps/{app_name}/dynos'
 
     # Headers with authorization and content type
     headers = {
         'Accept': 'application/vnd.heroku+json; version=3',
-        'Authorization': f'Bearer {api_key}',
-        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {api_key}'
     }
 
     # Get a list of dynos
@@ -45,6 +38,6 @@ def dynos(api_key, app_name):
         dyno_id = dyno['id']
         restart_url = f'https://api.heroku.com/apps/{app_name}/dynos/{dyno_id}'
         requests.delete(restart_url, headers=headers)
-        sendmessage("Dynos Restarted")
+    editMessage(reply, "Dynos Restarted!",)
         
-bot.add_handler(MessageHandler(dynos, filters=command(BotCommands.DynosCommand) & CustomFilters.sudo))
+bot.add_handler(MessageHandler(restart_dynos, filters=command(BotCommands.DynosCommand) & CustomFilters.sudo))
