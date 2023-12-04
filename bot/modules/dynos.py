ef5a1f73f9fd60f8be067dd5414ad88421aa7bea
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-05 00:59:22 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -52,18 +52,20 @@ async def restart_dynos(_, message):
         requests.delete(restart_url, headers=headers)
     await editMessage(reply, "Dynos Restarted!")
 
-async def func(url, payload, auth_header):
-    headers = {"Authorization": auth_header, "Referer": url}
-    encrypted_response = requests.post(url, data=payload, headers=headers)  # Corrected variable name
+async def func(link, payload, auth_header):
+    headers = {"Authorization": auth_header, "Referer": link}
+    encrypted_response = requests.post(link, data=payload, headers=headers)  # Corrected variable name
     decoded_data = base64.b64decode(encrypted_response.text[::-1][24:-20]).decode("utf-8")
     return json.loads(decoded_data)
 
 async def index(_, message):  # Added 'message' parameter
+    args = message.text.split()
+    link = args[1] if len(args) > 1 else ''
     reply = await sendMessage(message, "Extracting Index...")    
-    url = f"{url}/" if url[-1] != '/' else url
+    link = f"{link}/" if link[-1] != '/' else link
     auth_header = f"Basic {base64.b64encode('username:password'.encode()).decode().strip()}"
     payload = {"page_token": "", "page_index": 0}  # Assuming next_page_token is not needed here
-    decrypted_response = await func(url, payload, auth_header)  # Corrected function call
+    decrypted_response = await func(link, payload, auth_header)  # Corrected function call
     if "data" in decrypted_response and "files" in decrypted_response["data"]:
         size = [humanize.naturalsize(urllib.parse.quote(file["size"])) for file in decrypted_response["data"]["files"] if file["mimeType"] != "application/vnd.google-apps.folder"]
         result = '\n'.join(["\nName:" + urllib.parse.unquote(file["name"]) + "  " + s + "\nhttps://drive.google.com/file/d/" + urllib.parse.quote(file["id"]) for file, s in zip(decrypted_response["data"]["files"], size) if file["mimeType"] != "application/vnd.google-apps.folder"])
