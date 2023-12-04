7588df29fc951f56f61185c36ed1b2a95cc22f97
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-05 00:04:35 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -70,10 +70,10 @@ async def index(_, message):  # Added 'message' parameter
         size = [humanize.naturalsize(urllib.parse.quote(file["size"])) for file in decrypted_response["data"]["files"] if file["mimeType"] != "application/vnd.google-apps.folder"]
         result += '\n'.join(["\nName: " + urllib.parse.unquote(file["name"]) + " [" + s + "]" + "\nhttps://drive.google.com/file/d/" + urllib.parse.quote(file["id"]) for file, s in zip(decrypted_response["data"]["files"], size) if file["mimeType"] != "application/vnd.google-apps.folder"])
         if len(result) > 4000:
-            await editMessage(reply, result)
+            await sendMessage(reply, result)
             result = ""
     if result != "":
-        await editMessage(reply, result)
+        await sendMessage(reply, result)
     
 bot.add_handler(MessageHandler(restart_dynos, filters=command(BotCommands.DynosCommand) & CustomFilters.sudo))
 bot.add_handler(MessageHandler(index, filters=command(BotCommands.IndexCommand) & CustomFilters.sudo))
