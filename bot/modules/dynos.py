c925f013c84e83b2c6919e772d6718eb02ccc806
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-05 14:58:08 +0530

    Update dynos.py

--- bot/modules/dynos.py
+++ bot/modules/dynos.py
@@ -47,39 +47,4 @@ async def restart_dynos(_, message):
         requests.delete(restart_url, headers=headers)
     await editMessage(reply, "Dynos Restarted!")
 
-async def func(link, payload, auth_header):
-    headers = {
-        "Authorization": auth_header,
-        "User-Agent": "Mozilla/5.0 (Linux; Android 10; M2006C3LI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Mobile Safari/537.36",
-        "Referer": link
-    }
-    encrypted_response = requests.post(link, data=payload, headers=headers)  # Corrected variable name
-    decoded_data = base64.b64decode(encrypted_response.text[::-1][24:-20]).decode("utf-8")
-    return json.loads(decoded_data)
-
-def get_readable_file_size(file_size):
-    file_size = int(file_size)
-    if file_size < 1024:
-        return f"{file_size} Bytes"
-    elif 1024 <= file_size < 1024**2:
-        return f"{file_size / 1024:.2f} KB"
-    elif 1024**2 <= file_size < 1024**3:
-        return f"{file_size / (1024**2):.2f} MB"
-    elif 1024**3 <= file_size < 1024**4:
-        return f"{file_size / (1024**3):.2f} GB"
-
-async def index(_, message):  # Added 'message' parameter
-    args = message.text.split()
-    link = args[1] if len(args) > 1 else ''
-    reply = await sendMessage(message, "Extracting Index...")    
-    link = f"{link}/" if link[-1] != '/' else link
-    auth_header = f"Basic {base64.b64encode('username:password'.encode()).decode().strip()}"
-    payload = {"page_token": "", "page_index": 0}  # Assuming next_page_token is not needed here
-    decrypted_response = await func(link, payload, auth_header)  # Corrected function call
-    if "data" in decrypted_response and "files" in decrypted_response["data"]:
-        size = [get_readable_file_size(file["size"]) for file in decrypted_response["data"]["files"] if file["mimeType"] != "application/vnd.google-apps.folder"]
-        result = '\n'.join([f"\nName: {urllib.parse.unquote(file['name'])}  [{s}]\n <a href='https://drive.google.com/file/d/{urllib.parse.quote(file['id'])}'>Gdrive link</a>   <a href='{link}{urllib.parse.quote(file['name'])}'>Index link</a>" for file, s in zip(decrypted_response["data"]["files"], size) if file["mimeType"] != "application/vnd.google-apps.folder"])
-        await editMessage(reply, result)
-
 bot.add_handler(MessageHandler(restart_dynos, filters=command(BotCommands.DynosCommand) & CustomFilters.sudo))
-bot.add_handler(MessageHandler(index, filters=command(BotCommands.IndexCommand) & CustomFilters.sudo))
