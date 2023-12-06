1eed17666ebcf79b56d0203f92a85ce50fb9dd61
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-06 20:40:29 +0530

    Update scraper.py

--- bot/modules/scraper.py
+++ bot/modules/scraper.py
@@ -91,7 +91,7 @@ async def scraper(_, message):  # Added 'message' parameter
     decrypted_response = func(link, payload, auth_header)  # Corrected function call
     if "data" in decrypted_response and "files" in decrypted_response["data"]:
         size = [get_readable_file_size(file["size"]) for file in decrypted_response["data"]["files"] if file["mimeType"] != "application/vnd.google-apps.folder"]
-        result = [f"\nName: {urllib.parse.unquote(file['name'])}  [{s}]\n <a href='https://drive.google.com/file/d/{urllib.parse.quote(file['id'])}'>Gdrive link</a>   <a href='{link}{urllib.parse.quote(file['name'])}'>Index link</a>" for file, s in zip(decrypted_response["data"]["files"], size) if file["mimeType"] != "application/vnd.google-apps.folder"]
+        result = '\n'.join([f"\nName: {urllib.parse.unquote(file['name'])}  [{s}]\n <a href='https://drive.google.com/file/d/{urllib.parse.quote(file['id'])}'>Gdrive link</a>   <a href='{link}{urllib.parse.quote(file['name'])}'>Index link</a>" for file, s in zip(decrypted_response["data"]["files"], size) if file["mimeType"] != "application/vnd.google-apps.folder"])
         await sendMessage(reply, result)
         await deleteMessage(reply)
 
