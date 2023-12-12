52c03064545e82cc60e08785c64941ec9da1bba5
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-13 00:17:45 +0530

    Update query.py

--- bot/modules/query.py
+++ bot/modules/query.py
@@ -37,12 +37,20 @@ async def search_anime(query, message):
     reply = await sendMessage(message, "Searching.....")
     base_url = "https://www9.gogoanimes.fi/search.html?keyword="
     search_url = f"{base_url}{query}"
-    soup = soup_res(search_url)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(search_url, headers=headers)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
     links = soup.find_all('a', href=re.compile(r'.*/category/.*'))
+
+    unique_links = set()  # Use a set to store unique links
+
     for r in links:
         anime_href = r['href']
-        anime_link = ""
-        anime_link += f"https://www9.gogoanimes.fi{anime_href}"
+        anime_link = f"https://www9.gogoanimes.fi{anime_href}"
+        result = ""
+        result += f"{anime_link}\n"
         await editMessage(reply, anime_link)
         
 def arg_parser(items, arg_base):
