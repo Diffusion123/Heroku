f51de49a192c5a4ba77e6c7a3dbce3b95e7bd79c
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-13 09:46:27 +0530

    Update query.py

--- bot/modules/query.py
+++ bot/modules/query.py
@@ -17,31 +17,24 @@ from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, d
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
+def soup_res(link):
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(link, headers=headers)
+    return BeautifulSoup(response.content, 'html.parser')
+
 async def query_search(_, message):
-    buttons = ButtonMaker()
-    buttons.ubutton("🔎 VIEW")
-    button = buttons.build_menu(1)
-    await sendMessage(message, button)
-    
-async def search_anime(query, message):
+    args = message.text.split()
+    link = args[1] if len(args) > 1 else ''
     reply = await sendMessage(message, "Searching.....")
     base_url = "https://www9.gogoanimes.fi/search.html?keyword="
     search_url = f"{base_url}{query}"
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(search_url, headers=headers)
-
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = soup_res(search_url)
     links = soup.find_all('a', href=re.compile(r'.*/category/.*'))
-
-    unique_links = set()  # Use a set to store unique links
-
     for r in links:
         anime_href = r['href']
         anime_link = f"https://www9.gogoanimes.fi{anime_href}"
         result = ""
         result += f"{anime_link}\n"
-        await editMessage(reply, anime_link)
-
+        await editMessage(reply, result)
 
 bot.add_handler(MessageHandler(query_search, filters=command(BotCommands.QueryCommand) & CustomFilters.sudo))
