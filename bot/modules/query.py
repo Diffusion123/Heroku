2cf5fc774020931c9c773297517a11105cbe4c99
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-13 07:42:01 +0530

    Update query.py

--- bot/modules/query.py
+++ bot/modules/query.py
@@ -15,21 +15,20 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, deleteMessage
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
+from bot.helper.telegram_helper.button_build import ButtonMaker
 
 async def query_search(_, message):
-    text = message.text.split('\n')
-    input_list = text[0].split(' ')
-
-    arg_base = {'word': '', '-g': ''}
-    args = arg_parser(input_list[1:], arg_base)
-    cmd = input_list[0].split('@')[0]
-
-    word = args['word']
-    gogo_animes = args['-g']  # Renamed the variable
-
-    if gogo_animes:
-        await search_anime(cmd, message)  # Pass the correct variable to the function
-    
+    btns = ButtonMaker()
+    btns.ibutton('Back', f'wzmlx {user_id} stats home')
+    if key == "home":
+        btns = ButtonMaker()
+        btns.ibutton('Bot Stats', f'wzmlx {user_id} stats stbot')
+        btns.ibutton('OS Stats', f'wzmlx {user_id} stats stsys')
+        btns.ibutton('Repo Stats', f'wzmlx {user_id} stats strepo')
+        btns.ibutton('Bot Limits', f'wzmlx {user_id} stats botlimits')
+        msg = "⌬ <b><i>Bot & OS Statistics!</i></b>"
+    btns.ibutton('Close', f'wzmlx {user_id} close')
+    return msg, btns.build_menu(2)
     
 async def search_anime(query, message):
     reply = await sendMessage(message, "Searching.....")
@@ -50,45 +49,6 @@ async def search_anime(query, message):
         result = ""
         result += f"{anime_link}\n"
         await editMessage(reply, anime_link)
-        
-def arg_parser(items, arg_base):
-    if not items:
-        return arg_base
-    bool_arg_set = {'-g', '-e', '-z', '-s', '-j', '-d'}
-    t = len(items)
-    i = 0
-    arg_start = -1
-
-    while i + 1 <= t:
-        part = items[i].strip()
-        if part in arg_base:
-            if arg_start == -1:
-                arg_start = i
-            if i + 1 == t and part in bool_arg_set or part in ['-s', '-j']:
-                arg_base[part] = True
-            else:
-                sub_list = []
-                for j in range(i + 1, t):
-                    item = items[j].strip()
-                    if item in arg_base:
-                        if part in bool_arg_set and not sub_list:
-                            arg_base[part] = True
-                        break
-                    sub_list.append(item.strip())
-                    i += 1
-                if sub_list:
-                    arg_base[part] = " ".join(sub_list)
-        i += 1
-
-    link = []
-    if items[0].strip() not in arg_base:
-        if arg_start == -1:
-            link.extend(item.strip() for item in items)
-        else:
-            link.extend(items[r].strip() for r in range(arg_start))
-        if link:
-            arg_base['link'] = " ".join(link)
-    return arg_base
 
 
 bot.add_handler(MessageHandler(query_search, filters=command(BotCommands.QueryCommand) & CustomFilters.sudo))
