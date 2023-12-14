75da6cd0dcc6e107266fe8c36a7d6a5f6abcd844
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2023-12-15 01:36:37 +0530

    Update query.py

--- bot/modules/query.py
+++ bot/modules/query.py
@@ -9,7 +9,6 @@ from urllib.parse import unquote, urlparse
 
 from pyrogram.handlers import MessageHandler
 from pyrogram.filters import command
-from bot.modules.scraper import gogoanimes
 from bot import bot
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
@@ -17,28 +16,66 @@ from bot.helper.telegram_helper.message_utils import editMessage, sendMessage, d
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from bot.helper.telegram_helper.button_build import ButtonMaker
 
-def soup_res(link):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    response = requests.get(link, headers=headers)
+def last_episode(url):
+    soup = soup_res(url)
+    return soup.find('a', class_='active')['ep_end']
+
+def generate_episode_urls(base_url, format, num_episodes):
+    episode_urls = []
+    for episode in range(1, num_episodes + 1):
+        episode_url = f"{format}{episode}"
+        episode_urls.append(episode_url)
+    return episode_urls
+
+def get_links(episode_url):
+    soup = soup_res(episode_url)
+    links = soup.find_all('a', {'data-video': re.compile(r'https://dood.*\/.*')})
+    title_tag = soup.find('title')
+    if title_tag:
+        title_text = title_tag.get_text()
+        text_part = title_text.replace("Watch ", "").replace(" at Gogoanime", "")
+    else:
+        text_part = "Title not available"
+    for c in links:
+        url_link = c['data-video']
+        r = url_link.replace("/e/", "/d/")
+        return f"{text_part}\nWatch Online:- <a href='{url_link}'>1080P Dood-HD</a>  <a href='{r}'>Download Link</a>\n\n"
+        
+def soup_res(url):
+    response = requests.get(url)
     return BeautifulSoup(response.content, 'html.parser')
 
+def gogoanimes(l):
+    new_url = l.split("/")[4]
+    m_url = l.split("/")[2]
+    each_url = f"https://{m_url}/{new_url}-episode-"
+    num_episodes = int(last_episode(l))
+    episode_urls = generate_episode_urls(l, each_url, num_episodes)
+    for episode_url in episode_urls:
+        return get_links(episode_url)
+        
 async def query_search(_, message):
     args = message.text.split()
     link = args[1] if len(args) > 1 else ''
     reply = await sendMessage(message, "Searching.....")
     base_url = "https://www9.gogoanimes.fi/search.html?keyword="
-    search_url = f"{base_url}{link}"
+    search_url = f"{base_url}{query}"
+
     soup = soup_res(search_url)
     links = soup.find_all('a', href=re.compile(r'.*/category/.*'))
-    
+
     unique_links = set()
-    
+    results = ""
     for r in links:
         anime_href = r['href']
         anime_link = f"https://www9.gogoanimes.fi{anime_href}"
-        unique_links.add(anime_link)
+        unique_links.add(anime_link)  # Add each unique link to the set
 
-    result = unique_links
-    await gogoanimes(result, message)
-
+    for result in unique_links:
+        results += gogoanimes(result)
+        await editMessage(reply, results)
+        if len(results) > 4000:
+            sent = await sendMessage(reply, results)
+            result = ""
+            
 bot.add_handler(MessageHandler(query_search, filters=command(BotCommands.QueryCommand) & CustomFilters.sudo))
