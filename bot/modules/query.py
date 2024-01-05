42885fe2b07caaf638a2c39bf2031d6e3d9e7215
Diffusion 123 <133562822+Diffusion123@users.noreply.github.com>
2024-01-05 22:15:28 +0530

    Added hindi songs scrape

--- bot/modules/query.py
+++ bot/modules/query.py
@@ -133,7 +133,7 @@ async def pagalhindi(message):
 
         for s_link in s_links:
             s_result = s_link['href']
-            new_link = qoute(s_result).replace("%3A",":")
+            new_link = quote(s_result).replace("%3A",":")
             t = s_result.replace("128-", "").replace("120-", "").replace("192-", "").replace("320-", "").split('/')
             result += f"Name: {t[4]}\n <a href='({t}'>Download Link</a>\n"
             await editMessage(reply, result)
